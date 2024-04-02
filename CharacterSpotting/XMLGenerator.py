a6f4c9c6d6e9a257aa4df649f204f73b22fdd9b8
lunaalvareez <815976@unizar.es>
2024-04-02 19:33:44 +0200

    Update XMLGenerator.py
    Xml generator hecho

--- CharacterSpotting/XMLGenerator.py
+++ CharacterSpotting/XMLGenerator.py
@@ -5,6 +5,7 @@ import GlobalConstants as paths
 
 source_dir = paths.words_source_RIMES #From RIMES dataset
 transcription_dir = paths.words_transcription_RIMES #From RIMES dataset
+xml_dir = "/xml"
 
 for filename in os.listdir(transcription_dir):
     # Create the root element <form>
@@ -19,23 +20,34 @@ for filename in os.listdir(transcription_dir):
     with open(file_path, 'r') as f:
         lines = f.readlines()
 
-    words = []
+    # words = []
 
     # Create the subelement <machine-printed-part>
     machine_printed_part = ET.SubElement(form, "machine-printed-part")
 
     for line_text in lines:
-        line = ET.SubElement(machine_printed_part, "machine-print-line")
-        line.set("text", line_text)
-        words.extend(line_text.split(" "))
+        print_line = ET.SubElement(machine_printed_part, "machine-print-line")
+        print_line.set("text", line_text)
+        # words.extend(line_text.split(" "))
 
     # Create the subelement <handwritten-part>
     handwritten_part = ET.SubElement(form, "handwritten-part")
 
     # TODO: Add the line and words
 
+    for i, line_text in lines:
+        line = ET.SubElement(handwritten_part, "line")
+        line.set("id", f"{id}-{i:02}")
+        line.set("text", line_text)
+        words = line_text.split(" ")
+        for j, word_text in words:
+            word = ET.SubElement(line, "word")
+            word.set("id", f"{id}-{i:02}-{j:02}")
+            word.set("text", word_text)
+
     # Create the XML tree
     tree = ET.ElementTree(form)
 
     # Write the XML tree in a file
-    tree.write(id + ".xml", encoding="ISO-8859-1", xml_declaration=True, doctype='<!DOCTYPE form SYSTEM "http://www.iam.unibe.ch/~fki/iamdb/form-metadata.dtd">')
+    xml_path = os.path.join(xml_dir, id+".xml")
+    tree.write(xml_path, encoding="ISO-8859-1", xml_declaration=True, doctype='<!DOCTYPE form SYSTEM "http://www.iam.unibe.ch/~fki/iamdb/form-metadata.dtd">')
